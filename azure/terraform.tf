787ad4dc55326cc8a988d9f5e85fbadcfe939d9a
Oleksandr <59727267+okrasnoporov-clgx@users.noreply.github.com>
2025-09-19 12:09:49 +0300

    Create terraform.tf

---
+++ azure/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "4.45.0"
+          }
+    }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscribtion_id
+  tenant_id       = var.tenant_id
+  features {
+  }
+}
